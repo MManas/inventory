0b2577b2d452bf5cc146f79eb593c07f48a1ef46
MManas <mahajanm02@gmail.com>
2018-11-25 18:44:10 -0500

    Fixed Some Errors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname+'/routes/index.html');
 })
 app.get('/inventory', (req,res) => {
-    res.sendFile(__dirname+"/inventory.json");
+    res.sendFile(__dirname+"/cad.json");
 })
 app.get('/cad.html', (req,res) => {
     res.sendFile(__dirname+"/routes/cad.html");
